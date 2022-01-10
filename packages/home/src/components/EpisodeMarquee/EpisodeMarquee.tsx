21745204c6bc6863cb7c14861b66e5faeb6ad88b
Shun Kakinoki <shunkakinoki@gmail.com>
2022-01-09 17:53:14 -0800

    feat: ini episode marquee

---
+++ packages/home/src/components/EpisodeMarquee/EpisodeMarquee.tsx
@@ -0,0 +1,64 @@
+import type { FC } from "react";
+import Marquee from "react-fast-marquee";
+
+import {
+  APPLE_URL,
+  DISCORD_URL,
+  NEWSLETTER_URL,
+  SPOTIFY_URL,
+  TWITTER_URL,
+} from "@/home/const/config";
+
+export type EpisodeMarqueeProps = {
+  direction?: "right" | "left";
+};
+
+export type EpisodeExternalLinkProps = {
+  href: string;
+};
+
+export const EpisodeExternalLink: FC<EpisodeExternalLinkProps> = ({
+  href,
+  children,
+}) => {
+  return (
+    <a
+      className="mx-4 text-xl uppercase hover:animate-pulse"
+      target="_blank"
+      rel="noreferrer"
+      href={href}
+    >
+      <span>{children}</span>
+    </a>
+  );
+};
+
+export const EpisodeMarquee: FC<EpisodeMarqueeProps> = ({
+  direction = "left",
+}) => {
+  return (
+    <Marquee
+      pauseOnHover
+      direction={direction}
+      gradient={false}
+      speed={30}
+      className="py-2 text-white bg-gradient-to-bl from-indigo-300 to-purple-400"
+    >
+      <EpisodeExternalLink href={APPLE_URL}>
+        Listen on Apple Podcast
+      </EpisodeExternalLink>
+      <EpisodeExternalLink href={NEWSLETTER_URL}>
+        New Newsletter Everyweek
+      </EpisodeExternalLink>
+      <EpisodeExternalLink href={SPOTIFY_URL}>
+        Listen on Spotify
+      </EpisodeExternalLink>
+      <EpisodeExternalLink href={TWITTER_URL}>
+        Follow on Twitter
+      </EpisodeExternalLink>
+      <EpisodeExternalLink href={DISCORD_URL}>
+        Join our Discord
+      </EpisodeExternalLink>
+    </Marquee>
+  );
+};
--- packages/home/src/pages/index.tsx
+++ packages/home/src/pages/index.tsx
@@ -1,14 +1,19 @@
+import { EpisodeMarquee } from "@/home/components/EpisodeMarquee";
 import { FooterLogo } from "@/home/components/FooterLogo";
 import { GlowLogo } from "@/home/components/GlowLogo";
 
 export const IndexPage = (): JSX.Element => {
   return (
-    <div className="w-full min-h-screen bg-gradient-to-bl from-indigo-300 via-blue-300 to-purple-300">
-      <div className="flex flex-col justify-center items-center space-y-24 h-screen">
-        <GlowLogo src="cover.png" />
-        <FooterLogo />
+    <>
+      <EpisodeMarquee />
+      <div className="w-full h-[calc(100vh-5.5rem)] bg-gradient-to-bl from-indigo-300 via-blue-300 to-purple-300 border border-white">
+        <div className="flex flex-col justify-center items-center space-y-24 h-full">
+          <GlowLogo src="cover.png" />
+          <FooterLogo />
+        </div>
       </div>
-    </div>
+      <EpisodeMarquee direction="right" />
+    </>
   );
 };
 
