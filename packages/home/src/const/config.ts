3c89a958f56c3676e86e09d7faa2f139480c212c
Shun Kakinoki <39187513+shunkakinoki@users.noreply.github.com>
2022-01-19 23:05:50 -0800

    Update config.ts

--- packages/home/src/const/config.ts
+++ packages/home/src/const/config.ts
@@ -1,4 +1,4 @@
-export const DISCORD_URL = "https://discord.com/invite/BygDhCVW";
+export const DISCORD_URL = "https://discord.com/invite/9VTzKygcBG";
 export const GITHUB_URL = "https://github.com/TWiDAOs/TWiDAOs";
 export const TWITTER_URL = "https://twitter.com/TWiDAOs";
 export const ANCHOR_URL = "https://anchor.fm/twidaos";
